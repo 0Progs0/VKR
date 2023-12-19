4c946809ecf516c51d56d22b90cf33cbc6d6a017
0Progs0 <105976327+0Progs0@users.noreply.github.com>
2023-12-19 17:04:19 +0300

    api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,7 +11,7 @@ const generateJwt = (id, name, email, role) => {
     )
 }
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
         const {name, email, password, role} = req.body
         if (!name || !email || !password) {
             return next(ApiError.badRequest('Некоректное заполнение полей!'))
@@ -26,16 +26,23 @@ class UserController {
         return res.json({token})
     }
 
-    async login(req, res) {
-
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internal('Пользователь не найден'))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal('Неверный пароль'))
+        }
+        const token = generateJwt(user.id, user.name, user.email, user.role)
+        return res.json({token})
     }
 
     async check(req, res, next) {
-        const {id} = req.query
-        if (!id) {
-            return next(ApiError.badRequest('Не указан ID'))
-        }
-        res.json(id)
+        const token = generateJwt(req.user.id, req.user.name, req.user.email, req.user.role)
+        return res.json({token})
     }
 }
 
